d2d50cd820e783309ff933860447de2bb82ac22e
Dave Springer <david.k.springer@colorado.edu>
2019-03-11 19:48:01 -0400

    Created Menu class

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,51 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=1, message = "Category Name must not be empty")
+    private String name;
+
+    @ManyToMany
+    private List<Cheese> cheeses;
+
+    public Menu() {
+    }
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void add(Cheese cheese) {
+        cheeses.add(cheese);
+    }
+}
