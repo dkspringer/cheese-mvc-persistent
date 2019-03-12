bd39ac65cbfdf88ee65d6541a259506ef348ae93
Dave Springer <david.k.springer@colorado.edu>
2019-03-11 20:31:14 -0400

    Created and handled form to add new menu

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,14 +1,21 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping(value = "menu")
+@RequestMapping(value="menu")
 public class MenuController {
 
     @Autowired
@@ -17,12 +24,30 @@ public class MenuController {
     @Autowired
     private CheeseDao cheeseDao;
 
-    @RequestMapping(value = "")
+    @RequestMapping(value="")
     public String index(Model model) {
         model.addAttribute("title", "Menus");
         model.addAttribute("menus", menuDao.findAll());
 
         return "menu/index";
+    }
+
+    @RequestMapping(value="add", method=RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute("menu", new Menu());
+
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+        menuDao.save(menu);
+        return "redirect:";
     }
 
 }
